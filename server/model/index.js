d3fe8406afc1592aa03bdbd258153880354bb84f
liangwnele <502635097@qq.com>
2017-11-14 21:06:28 +0800

    xiu

--- server/model/index.js
+++ server/model/index.js
@@ -1,7 +1,17 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test", { useMongoClient: true }).then(
+  () => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+    console.log(`已经连接数据库 localhost/test`)
+  },
+  err => {
+    /** handle initial connection error */
+    console.error(`连接数据库 localhost/test 失败！！`)
+    
+  }
+);
 mongoose.Promise = global.Promise;
 
 module.exports = {
-  user:require('./user')
-}
+  user: require("./user")
+};
--- server/service/user.js
+++ server/service/user.js
@@ -1,15 +1,8 @@
- const db = require("../model");
- class User {
-
-  async add (ctx){
-<<<<<<< HEAD
-    consoel.log(ctx)
-    let user = db.user.find({name:"123456"})
-=======
-    let user = db.user.find({name:"12"})
->>>>>>> b7ccaf0c7badad1ce42d3a81c4ef55fa17d41d97
+const db = require("../model");
+class User {
+  async add(ctx) {
+    let user = db.user.find({ name: "12" });
   }
-
 }
 
-module.exports  = new User()
+module.exports = new User();
