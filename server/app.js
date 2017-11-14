3027948a737bb46b7b56844aba45affc1b48f13d
liangwnele <502635097@qq.com>
2017-11-14 17:13:28 +0800

    xiug

--- server/app.js
+++ server/app.js
@@ -1,8 +1,32 @@
-const Koa = require('koa');
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
+
+const router = require("./routes");
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
-
-app.listen(3000);
+app
+  .use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set("X-Response-Time", `${ms}ms`);
+  })
+  .use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms} ms`);
+  })
+  .use(cors())
+  .use(bodyParser())
+  .use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (e) {
+      ctx.body = "fff";
+    }
+  })
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(3000);
---
+++ server/routes/index.js
@@ -0,0 +1,25 @@
+const Router = require("koa-router");
+const router = new Router();
+
+router
+  .get("/", async (ctx, next) => {
+    ctx.body =  ctx.request.body;
+  })
+  .post("/users", async (ctx, next) => {
+    // ...
+    ctx.body = ctx.request.body;
+  })
+  .put("/users/:id", async (ctx, next) => {
+    // ...
+    ctx.body = ctx.request.body;
+  })
+  .del("/users/:id", async (ctx, next) => {
+    // ...
+    ctx.body = ctx.request.body;
+  })
+  .all("/users/:id", async (ctx, next) => {
+    // ...
+    ctx.body = ctx.request.body;
+  });
+
+module.exports = router;
